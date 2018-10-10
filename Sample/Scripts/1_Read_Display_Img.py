2b67ed89255ebd1df2bb28d58058a8161a01c4dc
shubhu45 <shubhamnikam333@gmail.com>
2018-10-10 19:23:09 +0530

    Script to Read and Display image

--- Sample/Scripts/1_Read_Display_Img.py
+++ Sample/Scripts/1_Read_Display_Img.py
@@ -1,13 +1,13 @@
 
 # Author - Shubham Nikam
 
-# Basic Program of Displaying image
+# Script to Read and Display image
 
 import cv2
 
 img = cv2.imread('baboon.png')
 
-cv2.imshow('Baboon_Image',img)
+cv2.imshow('Baboon Image',img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
