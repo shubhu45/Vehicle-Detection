bff6104f6cc4d54a984a79b1a9f642717bb0b430
shubhu45 <shubhamnikam333@gmail.com>
2018-10-10 19:25:21 +0530

    Script to convert image from BGR to Gray

--- Sample/Scripts/4_BGR2GRAY.py
+++ Sample/Scripts/4_BGR2GRAY.py
@@ -7,11 +7,12 @@ import cv2
 
 img = cv2.imread('baboon.png')
 
-cv2.imshow('Baboon_Image',img)
+cv2.imshow('Baboon Image',img)
 cv2.waitKey(0)
 
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Gray Scale Image",gray_img)
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
