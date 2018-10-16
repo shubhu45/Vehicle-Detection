81311f77e44fedede2d55991046cc60df9d1917b
Saurav Gore <sauravgore97@gmail.com>
2018-10-16 23:07:12 +0530

    Create vid_extract_savefrm.py

---
+++ Sample/Scripts/vid_extract_savefrm.py
@@ -0,0 +1,27 @@
+#Author - Saurav Gore
+
+#Extracting and Saving Video Frames
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+sg_cap = cv2.VideoCapture("/home/hduser/opencv/pro/vid/Episode (1).mkv")
+x,img = sg_cap.read()
+cnt = 0
+x = True
+while x:
+	ret,frame = sg_cap.read()
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	#hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+	cv2.imwrite("/home/hduser/opencv/pro/vid/vid_frames/frame%d.jpg" % cnt,img)
+	x,img = sg_cap.read()
+	print "Read a new frame:   " , x
+	cv2.imshow('frame',gray)
+	#cv2.imshow('frame',hsv)
+	cnt+=1
+    	if cnt == 1000:         				#break after 500 frames
+        	break
+
+sg_cap.release()
+cv2.destroyAllWindows()
